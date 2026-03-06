ded4927300cde795a72ed93cd215b27ee9dd050d
Christofer <cocokoola@gmail.com>
2026-03-06 19:04:33 +0200

    Added Lombok @Data to models

--- src/main/java/com/example/demo/model/Reservation.java
+++ src/main/java/com/example/demo/model/Reservation.java
@@ -1,9 +1,12 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 public class Reservation {
 
     @Id
--- src/main/java/com/example/demo/model/RestaurantTable.java
+++ src/main/java/com/example/demo/model/RestaurantTable.java
@@ -1,9 +1,12 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
 import java.util.List;
 
 @Entity
+@Data
 @Table(name = "restaurant_tables")
 public class RestaurantTable {
 
